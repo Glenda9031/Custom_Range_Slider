11b3322eeccff39958325c9c68b2a74d399be372
Glenda Reed <glendareed0807@sbcglobal.net>
2023-07-09 20:43:07 -0500

    Update index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const range = document.getElementById('range')
+
+range.addEventListener('input', (e) => {
+    const value = +e.target.value
+    const label = e.target.nextElementSibling
+
+    const range_width = getComputedStyle(e.target).getPropertyValue('width')
+    const label_width = getComputedStyle(label).getPropertyValue('width')
+})
